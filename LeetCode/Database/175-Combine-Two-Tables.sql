562982109e8acc34c6d8fa0926df26159fd97211
DYiKai <dengyk@turbur.com>
2019-04-01 15:26:09 +0800

    no message

---
+++ LeetCode/Database/175-Combine-Two-Tables.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT p.FirstName AS FirstName, p.LastName AS LastName, a.City AS City, a.State AS State FROM Person AS p LEFT JOIN Address AS a ON p.PersonId = a.PersonId
